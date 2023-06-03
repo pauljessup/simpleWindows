b4c16240f09c0782b4aafa0c176821bf7f140af0
pauljessup <paul.jessup@gmail.com>
2023-06-03 16:37:21 -0400

    The start of the main file

--- main.lua
+++ main.lua
@@ -1,2 +1,15 @@
+--all you need to do is require it, wherever you placed it.
 require("simpleWindows")
 
+
+function love.load() 
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
